369a8d2cbb3a7690a67dba837e7229535b95a4a7
prueba <prueba@gmail.com>
2025-11-18 13:45:36 +0000

    "mensaje"

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'exoress'
+import mongoose from 'mongoose'
+
+const Animal = mongoose.model('Animal', new mongoose.Schema({
+    tipo: String,
+    estado: String,
+}))
+
+const app = express()
+
+mongoose.connect('mongodb://user:password@localhost:27017/miapp?authSource=admin')
+
+app.get('/', async (_req, res) => {
+    console.log('listando...')
+    const animales = await Animal.find();
+    return res.send(animales)
+})
+
+app.get('/', async (requestAnimationFrame, res) => {
+    console.log('creando...')
+    await Animal.create({ tipo 'perro', estado 'feliz'})
+    return res.send('ok')
+
+})
+
+app.listen(3000, () => console.log('escuchando...'))
